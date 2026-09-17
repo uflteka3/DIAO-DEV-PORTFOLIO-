de03eb1ad64ca58e87837927cfadd95c933511d8
Ufl Teka <uflteka3@gmail.com>
2026-09-17 06:50:51 +0200

    Create SEO.tsx

---
+++ src/SEO.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+interface SEOProps {
+  title: string;
+  description: string;
+}
+
+export default function SEO({ title, description }: SEOProps) {
+  useEffect(() => {
+    document.title = title;
+
+    let meta = document.querySelector(
+      'meta[name="description"]'
+    ) as HTMLMetaElement | null;
+
+    if (!meta) {
+      meta = document.createElement("meta");
+      meta.name = "description";
+      document.head.appendChild(meta);
+    }
+
+    meta.content = description;
+  }, [title, description]);
+
+  return null;
+}
